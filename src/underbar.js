1aa08ff5eb5f59ab4cc769d9249727ada383a21f
dclay1983 <dclay1983@gmail.com>
2019-09-23 11:04:27 -0700

    Factor reduce

--- src/underbar.js
+++ src/underbar.js
@@ -120,6 +120,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    _.each(collection, function(item) {
+      result.push(iterator(item));
+    });
+    return result;
   };
 
   /*
@@ -161,6 +166,18 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    var noMemo = accumulator === undefined;
+    // track index if obbject
+    var index = 0;
+    _.each(collection, function(val) {
+      if (index === 0 && noMemo) {
+        accumulator = val
+      } else {
+        accumulator = iterator(accumulator, val);
+      }
+      index++
+    })
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
