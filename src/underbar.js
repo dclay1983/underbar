ca9806b37c992254d9aa4b14c232388d8c39a171
dclay1983 <dclay1983@gmail.com>
2019-09-25 15:54:39 -0700

    Factor extend and defaults

--- src/underbar.js
+++ src/underbar.js
@@ -235,12 +235,27 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.reduce( arguments, function (obj, argument) {
+      _.each(argument, function(val, key) {
+        obj[key] = val;
+      })
+      return obj;
+    })
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.reduce( arguments, function (obj, argument) {
+      _.each(argument, function(val, key) {
+        obj[key] = obj.hasOwnProperty(key) ? obj[key] : val;
+      })
+      return obj;
+    })
+    return obj;
   };
+  
 
 
   /**
